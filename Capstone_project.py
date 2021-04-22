0c5f46565c908aef6078f9e6f117d92a2d32e08b
Markus Taskila <78557554+Madfuuu@users.noreply.github.com>
2021-04-22 05:23:20 +0300

    Create Capstone_project.py

---
+++ Capstone_project.py
@@ -0,0 +1,4 @@
+#This notebook is for IBM Data Science Capstone project that finishes the 9 course data science package
+import pandas as pd
+import Numpy as np
+print("Hello Capstone Project Course!")
